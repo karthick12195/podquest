b62239339a64bebb5ee8b90acf4737edb992ea12
karthick12195 <karthick12195@gmail.com>
2024-05-05 16:33:13 -0700

    add text from podcast transcript

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import scripts.query_data as query_data
 
+conn = st.connection("snowflake")
+SP_SESSION = conn.session()
+
 def main():
     st.title("PodQuest: Use LLM to ask questions about science based tools from Huberman Lab podcast")
     # Input field for user question
@@ -10,10 +13,14 @@ def main():
     if st.button("Get Answer"):
         if user_question:
             # Call query_data.py function with user question as argument
-            answer = query_data.get_answer_from_prompt(user_question)
+            answer, context_text = query_data.get_answer_from_prompt(user_question, SP_SESSION)
             
             # Display answer
             st.write("Answer:", answer)
+
+            if answer != "Unable to find answers from Huberman Lab":
+                with st.expander("Show Relevant text from Podcast transcripts"):
+                    st.markdown(context_text)
         else:
             st.warning("Please enter a question")
 
